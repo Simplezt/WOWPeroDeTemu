93a7798d6fdfc2a0f5d2f1b8c20ee58233516118
Simplezt <Jb4ss0705@gmail.com>
2025-06-17 12:29:18 -0500

    Update

--- src/Model/Player.java
+++ src/Model/Player.java
@@ -1,4 +1,15 @@
 ﻿package Model;
 
+import java.util.ArrayList;
+
 public class Player {
+    private long playerID;
+    private String nickName;
+    private String password;
+
+    private String email;
+
+    ArrayList<Character> listOfCharacters = new ArrayList<>();
+
+
 }
