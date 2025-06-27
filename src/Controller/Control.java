739533cccbfd7b7e566195a66cac182d940bf2ed
Simplezt <Jb4ss0705@gmail.com>
2025-06-27 17:54:15 -0500

    Generated some logic for the creationg of players and their player ID

--- src/Controller/Control.java
+++ src/Controller/Control.java
@@ -1,8 +1,11 @@
 package Controller;
 
+import Model.Player;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Control {
@@ -12,5 +15,24 @@ public class Control {
 
     }
 
+    public void createNewPlayer(){
 
+        String nickname = "username";
+        String password = "password";
+        Player player = new Player(nickname, password);
     }
+
+    private Long generateRandomPlayerID(){
+        Long id = 1L;
+        for(int i = 0; listOfPlayers.size()> i; i++){
+            if(listOfPlayers.get(i).getPlayerID() == id){
+                id ++;
+            }
+        }
+        return id;
+    }
+
+    ArrayList<Player> listOfPlayers =  new ArrayList<>();
+    }
+
+
--- src/Model/Player.java
+++ src/Model/Player.java
@@ -3,6 +3,20 @@ package Model;
 import java.util.ArrayList;
 
 public class Player {
+
+    public Player(String nickName, String password) {
+        this.nickName = nickName;
+        this.password = password;
+    }
+
+    public void setPlayerID(long playerID) {
+        this.playerID = playerID;
+    }
+
+    public long getPlayerID() {
+        return playerID;
+    }
+
     private long playerID;
     private String nickName;
     private String password;
@@ -10,6 +24,8 @@ public class Player {
     private String email;
 
     ArrayList<Character> listOfCharacters = new ArrayList<>();
+
+
 
 
 }
