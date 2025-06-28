f9ffe0f4015ef30874b8bbba3e33d1454c749eb2
Simplezt <Jb4ss0705@gmail.com>
2025-06-27 19:44:18 -0500

    Created the player ID. Behaviour of creating player id: Increments in order, based on the last created ID. MergeSort algorithm to organize the player list based on player ID

--- src/Controller/Control.java
+++ src/Controller/Control.java
@@ -10,6 +10,7 @@ import java.util.StringTokenizer;
 
 public class Control {
 
+private Long latestIdCreated;
 
     public Control() {
 
@@ -19,17 +20,62 @@ public class Control {
 
         String nickname = "username";
         String password = "password";
+
         Player player = new Player(nickname, password);
+        player.setPlayerID(generatePlayerID());
+        listOfPlayers = organizePlayerList(listOfPlayers);
+
     }
 
-    private Long generateRandomPlayerID(){
-        Long id = 1L;
-        for(int i = 0; listOfPlayers.size()> i; i++){
-            if(listOfPlayers.get(i).getPlayerID() == id){
-                id ++;
-            }
+    private Long generatePlayerID(){
+        Long id = 1L; //Default value
+        if(listOfPlayers.size() == 0){
+            latestIdCreated = id;
+            return id;
         }
+        latestIdCreated++;
+        id = latestIdCreated;
         return id;
+    }
+
+    private ArrayList<Player> organizePlayerList(ArrayList<Player> list){
+        if(list.size() <2){
+            return list;
+        }
+        int mid = list.size()/2;
+        ArrayList<Player> left = new ArrayList<>(list.subList(0, mid));
+        ArrayList<Player> right = new ArrayList<>(list.subList(mid, list.size()));
+
+        return mergeList(organizePlayerList(left), organizePlayerList(right));
+
+    }
+
+    private ArrayList<Player> mergeList(ArrayList<Player> left, ArrayList<Player> right){
+        ArrayList<Player> result = new ArrayList<>();
+        int i = 0;
+        int j = 0;
+
+        while(i < left.size() && j < right.size()){
+            if(left.get(i).getPlayerID() <= right.get(j).getPlayerID()){
+                result.add(left.get(i));
+                i++;
+            }
+            else{
+                result.add(right.get(j));
+                j++;
+            }
+
+        }
+        while(i < left.size()){
+            result.add(left.get(i));
+            i++;
+        }
+        while(j < right.size()){
+            result.add(right.get(j));
+            j++;
+        }
+
+        return result;
     }
 
     ArrayList<Player> listOfPlayers =  new ArrayList<>();
